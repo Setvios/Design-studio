8a87707fee4515882386e5ef614c7248e7344a6a
Stanislav <setvios.it@gmail.com>
2017-08-25 18:57:47 +0300

    links on scroll effects added + small adaptive fixes

--- js/main.js
+++ js/main.js
@@ -15,19 +15,19 @@ $('a[href*="#"]')
 			location.hostname == this.hostname
 			) {
 			// Figure out element to scroll to
-	 var target = $(this.hash);
-	 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 			// Does a scroll target exist?
 			if (target.length) {
 				// Only prevent default if animation is actually gonna happen
 				event.preventDefault();
 				$('html, body').animate({
-					scrollTop: target.offset().top
+					scrollTop: target.offset().top - $('.navbar').height()
 				}, 1000)
 				return false;
-		 }
-	}
-});
+			}
+		}
+	});
 })
 
 // ############## INITIALIZE WOW AND ANIMATE ##############
@@ -35,3 +35,34 @@ $('a[href*="#"]')
 $(document).ready(function(){
 	new WOW().init();
 })
+
+
+// ############## HIGHLIGHT MENU ITEM ON SCROLL ##############
+
+$(document).ready(function() {
+    
+    $(window).scroll( function() {
+        
+        $("section").each(function()  {
+            
+            var sectionId = $(this).attr("id");
+            var height = $(this).outerHeight();
+            var topOffset = $(this).offset().top - $('.navbar').height() - 60;
+            
+            if ($(window).scrollTop() > topOffset && $(window).scrollTop() < topOffset + height) {
+             
+                $(".navbar-nav li a[href='#" + sectionId + "']").parent().addClass("active");
+           
+            } else {
+                $(".navbar-nav li a[href='#" + sectionId + "']").parent().removeClass("active");
+                
+            }
+            
+            
+        });
+    
+    
+    });
+    
+    
+});
